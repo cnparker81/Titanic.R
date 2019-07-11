bef428900a5aa8a8b80ca4cff77340715b411bda
cnparker81 <52382211+cnparker81@users.noreply.github.com>
2019-07-10 21:06:31 -0400

    Add files via upload

---
+++ Titanic.R
@@ -0,0 +1,16 @@
+str(titanic)
+
+# 2 - Use ggplot() for the first instruction
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  geom_bar(position = "dodge")
+
+# 3 - Plot 2, add facet_grid() layer
+ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+  facet_grid(. ~ Survived) + geom_bar(position = "dodge")
+
+# 4 - Define an object for position jitterdodge, to use below
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
+
+# 5 - Plot 3, but use the position object from instruction 4
+ggplot(titanic, aes(x = Pclass, y = Age, color = Sex)) +
+  facet_grid(. ~ Survived) + geom_point(size = 3, alpha = 0.5, position = posn.jd)
